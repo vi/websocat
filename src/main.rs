d0d695252d42b517a4296c355b38eae258981448
Vitaly _Vi Shukela <vi0oss@gmail.com>
2016-11-22 04:28:31 +0300

    Simplify the big way.

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,16 @@ extern crate error_chain;
 extern crate url;
 extern crate clap;
 
-const BUFSIZ : usize = 8192;
-const QSIZ : usize = 1;
-
 use std::thread;
-use std::sync::mpsc::sync_channel;
 use std::io::{stdin,stdout};
 
-use websocket::{Message, Sender, Receiver};
+use websocket::{Message, Sender, Receiver, DataFrame};
 use websocket::message::Type;
 use websocket::client::request::Url;
 use websocket::Client;
 
-use std::io::{Read,Error as IoError,ErrorKind as IoErrorKind,Write};
+use std::borrow::Borrow;
+use std::io::{Error as IoError, ErrorKind as IoErrorKind, Write};
 
 error_chain! {
     foreign_links {
@@ -51,18 +48,51 @@ impl<T: Sender> ::std::io::Write for SenderWrapper<T> {
     fn write(&mut self, buf: &[u8]) -> ::std::io::Result<usize> {
         let message = Message::binary(buf);
         let ret;
-        if buf.len() > 0 {
+        let len = buf.len();
+        if len > 0 {
+            debug!("Sending message of {} bytes", len);
             ret = self.0.send_message(&message);
         } else {
             // Interpret zero length buffer is request
             // to close communication
+            
+            debug!("Sending the closing message");
             ret = self.0.send_message(&Message::close());
         }
         ret.map_err(|e|IoError::new(IoErrorKind::BrokenPipe, e))?;
-        Ok(buf.len())
+        Ok(len)
     }
     fn flush(&mut self) -> ::std::io::Result<()> {
         Ok(())
+    }
+}
+
+struct ReceiverWrapper<T: Receiver<DataFrame>> (T);
+
+impl<T:Receiver<DataFrame>> ::std::io::Read for ReceiverWrapper<T> {
+    fn read(&mut self, buf: &mut [u8]) -> ::std::io::Result<usize> {
+        let ret = self.0.recv_message();
+        let msg : Message = ret.map_err(|e|IoError::new(IoErrorKind::BrokenPipe, e))?;
+        
+        match msg.opcode {
+            Type::Close => {
+                Ok(0)
+            }
+            Type::Ping => {
+                unimplemented!();
+            }
+            _ => {
+                let msgpayload : &[u8] = msg.payload.borrow();
+                let len = msgpayload.len();
+                debug!("Received message of {} bytes", len);
+                
+                assert!(buf.len() >= len);
+                
+                buf[0..len].clone_from_slice(msgpayload);
+                
+                Ok(len)
+            }
+        }
     }
 }
 
@@ -95,104 +125,29 @@ fn try_main() -> Result<()> {
 
     info!("Successfully connected");
 
-    let (mut sender, mut receiver) = response.begin().split();
-
-    //let (tx, rx) = sync_channel(QSIZ);
-
-    //let tx_1 = tx.clone();
-
-    /*let send_loop = thread::spawn(move || {
-        if let Err(er) = (|| -> Result<()> {
-            loop {
-                // Send loop
-                let message: Message = rx.recv()?;
-                if message.opcode == Type::Close { 
-                    break; 
-                }
-                sender.send_message(&message)?;
-            }
-            Ok(())
-        })() {
-            error!("Receive loop: {}", er);
-        }
-        let _ = sender.send_message(&Message::close());
-    });*/
+    let (sender, receiver) = response.begin().split();
+    let mut writer1 = SenderWrapper(sender);
+    let mut reader1 = ReceiverWrapper(receiver);
+    let mut writer2 = stdout();
+    let mut reader2 = stdin();
 
     let receive_loop = thread::spawn(move || -> Result<()> {
-        if let Err(x) = (|| -> Result<()> {
-            for m in receiver.incoming_messages() {
-                let message : websocket::Message = try!(m);
-                match message.opcode {
-                    Type::Close => {
-                        return Ok(());
-                    }
-                    Type::Ping => {
-                        /*tx_1.send(
-                            Message::pong(
-                                message.payload
-                            )).map_err(|_|"Failed pong")?*/
-                    }
-                    // Say what we received
-                    _ => {
-                        use std::borrow::Borrow;
-                        use std::io::Write;
-                        let msgpayload : &[u8] = message.payload.borrow();
-                        debug!("Received message of {} bytes", msgpayload.len());
-                        stdout().write_all(msgpayload)?;
-                        stdout().flush()?;
-                    }
-                }
-            }
-            Ok(())
-        })() {
-            error!("Error on receive loop: {}", x);
-        }
-        //let _ = tx_1.send(Message::close());
+        // Actual data transfer happens here
+        ::std::io::copy(&mut reader1, &mut writer2)?;
+        writer2.write(b"")?;
         Ok(())
     });
 
-    //let mut buffer : [u8; BUFSIZ] = [0; BUFSIZ];
-    
-    /*loop {
-        let data : Vec<u8> = match stdin().read(&mut buffer) {
-            Ok(0) => break,
-            Ok(ret) => {
-                debug!("Sending {} bytes of data", ret);
-                buffer[0..ret].to_vec()
-            }
-            Err(ref e) if e.kind() == IoErrorKind::Interrupted => continue,
-            Err(ref e) if e.kind() == IoErrorKind::WouldBlock  => continue,
-            Err(e) => {
-                error!("Read error: {}", e);
-                break;
-            }
-        };
-
-        let message = Message::binary(data);
-
-        match tx.send(message) {
-            Ok(()) => (),
-            Err(e) => {
-                error!("Main Loop: {:?}", e);
-                break;
-            }
-        }
-    }
-    let _ = tx.send(Message::close());*/
-
-    // We're exiting
-    
-    let mut wr = SenderWrapper(sender);
     
     // Actual data transfer happens here
-    ::std::io::copy(&mut stdin(), &mut wr)?;
-    
-    wr.write(b"")?; // Signal close
+    ::std::io::copy(&mut reader2, &mut writer1)?;
+    writer1.write(b"")?; // Signal close
 
     info!("Waiting for child threads to exit");
 
-    //let _ = send_loop.join();
-    let _ = receive_loop.join();
+    if let Err(x) = receive_loop.join() {
+        error!("Receiver loop delayed error: {:?}", x);
+    }
 
     info!("Exited");
     Ok(())
