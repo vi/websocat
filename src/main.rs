fc14864d2c05a540d8ffd51a3e2b9476d3efc367
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-01-14 04:13:07 +0300

    Fix read

--- src/main.rs
+++ src/main.rs
@@ -31,11 +31,6 @@ type WaitingForImplTraitFeature0 = tokio_io::codec::Framed<std::boxed::Box<webso
 type WaitingForImplTraitFeature1 = futures::stream::SplitStream<WaitingForImplTraitFeature0>;
 type WaitingForImplTraitFeature2 = futures::stream::SplitSink<WaitingForImplTraitFeature0>;
 
-struct WsReadWrapper(WaitingForImplTraitFeature1);
-
-impl AsyncRead for WsReadWrapper {
-
-}
 
 fn wouldblock<T>() -> std::io::Result<T> {
     Err(std::io::Error::new(std::io::ErrorKind::WouldBlock, ""))
@@ -49,9 +44,36 @@ fn io_other_error<E : std::error::Error + Send + Sync + 'static>(e:E) -> std::io
 
 mod my_copy;
 
+
+
+struct WsReadWrapper {
+    s: WaitingForImplTraitFeature1,
+    debt: Option<Vec<u8>>,
+}
+
+impl AsyncRead for WsReadWrapper {
+
+}
+
+impl WsReadWrapper {
+    fn process_message(&mut self, buf: &mut [u8], buf_in: &[u8]) -> std::result::Result<usize, std::io::Error> {
+        let l = buf_in.len().min(buf.len());
+        buf[..l].copy_from_slice(&buf_in[..l]);
+        
+        if l < buf_in.len() {
+            self.debt = Some(buf_in[l..].to_vec());
+        }
+        
+        Ok(l)
+    }
+}
+
 impl Read for WsReadWrapper {
     fn read(&mut self, buf: &mut [u8]) -> std::result::Result<usize, std::io::Error> {
-        match self.0.poll().map_err(io_other_error)? {
+        if let Some(debt) = self.debt.take() {
+            return self.process_message(buf, debt.as_slice())
+        }
+        match self.s.poll().map_err(io_other_error)? {
             Ready(Some(OwnedMessage::Close(_))) => {
                 brokenpipe()
             },
@@ -66,18 +88,10 @@ impl Read for WsReadWrapper {
                 Ok(0)
             }
             Ready(Some(OwnedMessage::Text(x))) => {
-                let buf_in = x.as_str().as_bytes();
-                let l = buf_in.len().min(buf.len());
-                buf[..l].copy_from_slice(&buf_in[..l]);
-                Ok(l)
-                // TODO
+                self.process_message(buf, x.as_str().as_bytes())
             }
             Ready(Some(OwnedMessage::Binary(x))) => {
-                let buf_in = x.as_slice();
-                let l = buf_in.len().min(buf.len());
-                buf[..l].copy_from_slice(&buf_in[..l]);
-                Ok(l)
-                // TODO
+                self.process_message(buf, x.as_slice())
             }
             NotReady => {
                 wouldblock()
@@ -131,31 +145,20 @@ fn run() -> Result<()> {
     let si = tokio_stdin_stdout::stdin(0);
     let so = tokio_stdin_stdout::stdout(0);
 
-    //let (usr_msg, stdin_ch) = mpsc::channel(0);
-    
     let runner = ClientBuilder::new(peeraddr.as_ref())?
         .add_protocol("rust-websocket")
         .async_connect(None, &core.handle())
         .and_then(|(duplex, _)| {
             let (sink, stream) = duplex.split();
             
-            let ws_str = WsReadWrapper(stream);
+            let ws_str = WsReadWrapper {
+                s: stream,
+                debt: None,
+            };
             let ws_sin = WsWriteWrapper(sink);
             
             handle.spawn(my_copy::copy(si, ws_sin).map(|_|()).map_err(|_|()));
             my_copy::copy(ws_str, so).map_err(|e| WebSocketError::IoError(e))
-            
-            /*stream.filter_map(|message| {
-                                  println!("Received Message: {:?}", message);
-                                  match message {
-                                      OwnedMessage::Close(e) => Some(OwnedMessage::Close(e)),
-                                      OwnedMessage::Ping(d) => Some(OwnedMessage::Pong(d)),
-                                      _ => None,
-                                  }
-                                 })
-                  .select(stdin_ch.map_err(|_| WebSocketError::NoDataAvailable))
-                  .forward(sink)
-            */
         });
     core.run(runner)?;
     Ok(())
