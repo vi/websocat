9b72888b7249d5a69cbd783bc793612808b11d72
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-05-27 06:39:38 +0300

    Refactor the test

--- tests/test.rs
+++ tests/test.rs
@@ -8,320 +8,182 @@ extern crate tokio_timer;
 use futures::future::Future;
 
 use tokio_core::reactor::Core;
-use websocat::{spec, WebsocatConfiguration};
+use websocat::{spec, WebsocatConfiguration, Options};
+
+fn dflt() -> Options {
+    Default::default()
+}
+
+macro_rules! wt {
+    ($core:ident, $s1:expr, $s2:expr, noopts, delay=$ms:expr) => {
+        wt!($core, $s1, $s2, opts=Default::default(), delay=$ms);
+    };
+    ($core:ident, $s1:expr, $s2:expr, noopts, nodelay) => {
+        wt!($core, $s1, $s2, opts=Default::default(), nodelay);
+    };
+    ($core:ident, $s1:expr, $s2:expr, opts=$opts:expr, delay=$ms:expr) => {{
+        let s1 = spec($s1).unwrap();
+        let s2 = spec($s2).unwrap();
+        let h2 = $core.handle();
+
+        let websocat = WebsocatConfiguration {
+            opts: $opts,
+            s1,
+            s2,
+        };
+
+        let t = tokio_timer::wheel().build();
+        let delay = t.sleep(std::time::Duration::new(0, $ms*1_000_000))
+            .map_err(|_| ());
+
+        delay.and_then(|()| {
+            wt!(wss, websocat, h2)
+        })
+    }};
+    ($core:ident, $s1:expr, $s2:expr, opts=$opts:expr, nodelay) => {{
+        let s1 = spec($s1).unwrap();
+        let s2 = spec($s2).unwrap();
+        let h2 = $core.handle();
+
+        let websocat = WebsocatConfiguration {
+            opts: $opts,
+            s1,
+            s2,
+        };
+      
+        wt!(wss, websocat, h2)
+    }};
+    (wss, $websocat:ident, $h2:ident) => {
+        $websocat.serve(
+                $h2,
+                std::rc::Rc::new(|e| {
+                    eprintln!("{}", e);
+                    panic!();
+                }),
+            )
+    };
+}
+
+macro_rules! prepare {
+    ($core:ident) => {
+        let _ = env_logger::try_init();
+        let mut $core = Core::new().unwrap();
+    };
+}
+macro_rules! run {
+    ($core:ident, $prog:expr) => {
+        $core.run($prog).map_err(|()| panic!()).unwrap();
+    };
+}
 
 #[test]
 fn trivial() {
-    let _ = env_logger::try_init();
-    let mut core = Core::new().unwrap();
-
-    let s1 = spec("literal:qwerty").unwrap();
-    let s2 = spec("assert:qwerty").unwrap();
-    let websocat = WebsocatConfiguration {
-        opts: Default::default(),
-        s1,
-        s2,
-    };
-
-    let prog = websocat.serve(
-        core.handle(),
-        std::rc::Rc::new(|_| {
-            panic!();
-        }),
-    );
-
-    core.run(prog).map_err(|()| panic!()).unwrap();
+    prepare!(core);
+    let prog = wt!(core,
+        "literal:qwerty",
+        "assert:qwerty",
+        noopts,
+        nodelay);
+    run!(core, prog);
 }
 
 #[test]
 fn tcp() {
-    let _ = env_logger::try_init();
-    let mut core = Core::new().unwrap();
-
-    let prog1 = {
-        let s1 = spec("literal:qwert2y").unwrap();
-        let s2 = spec("tcp-l:127.0.0.1:45912").unwrap();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        websocat.serve(
-            core.handle(),
-            std::rc::Rc::new(|e| {
-                eprintln!("{}", e);
-                panic!();
-            }),
-        )
-    };
-    let prog2 = {
-        let s1 = spec("tcp:127.0.0.1:45912").unwrap();
-        let s2 = spec("assert:qwert2y").unwrap();
-        let h2 = core.handle();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        let t = tokio_timer::wheel().build();
-        let delay = t.sleep(std::time::Duration::new(0, 200_000_000))
-            .map_err(|_| ());
-
-        delay.and_then(|()| {
-            websocat.serve(
-                h2,
-                std::rc::Rc::new(|e| {
-                    eprintln!("{}", e);
-                    panic!();
-                }),
-            )
-        })
-    };
+    prepare!(core);
+    let prog1 = wt!(core,
+        "literal:qwert2y",
+        "tcp-l:127.0.0.1:45912",
+        noopts,
+        nodelay);
+    let prog2 = wt!(core,
+        "tcp:127.0.0.1:45912",
+        "assert:qwert2y",
+        noopts,
+        delay=200);
 
     let prog = prog1.join(prog2);
-    core.run(prog).map_err(|()| panic!()).unwrap();
+    run!(core, prog);
 }
 
 #[test]
 fn ws() {
-    let _ = env_logger::try_init();
-    let mut core = Core::new().unwrap();
-
-    let prog1 = {
-        let s1 = spec("literal:qwert3y").unwrap();
-        let s2 = spec("ws-l:127.0.0.1:45913").unwrap();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        websocat.serve(
-            core.handle(),
-            std::rc::Rc::new(|e| {
-                eprintln!("{}", e);
-                panic!();
-            }),
-        )
-    };
-    let prog2 = {
-        let s1 = spec("ws://127.0.0.1:45913/ololo").unwrap();
-        let s2 = spec("assert:qwert3y").unwrap();
-        let h2 = core.handle();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        let t = tokio_timer::wheel().build();
-        let delay = t.sleep(std::time::Duration::new(0, 200_000_000))
-            .map_err(|_| ());
-
-        delay.and_then(|()| {
-            websocat.serve(
-                h2,
-                std::rc::Rc::new(|e| {
-                    eprintln!("{}", e);
-                    panic!();
-                }),
-            )
-        })
-    };
+    prepare!(core);
+    let prog1 = wt!(core,
+        "literal:qwert3y",
+        "ws-l:127.0.0.1:45913",
+        noopts,
+        nodelay);
+    let prog2 = wt!(core,
+        "ws://127.0.0.1:45913/ololo",
+        "assert:qwert3y",
+        noopts,
+        delay=200);
 
     let prog = prog1.join(prog2);
-    core.run(prog).map_err(|()| panic!()).unwrap();
+    run!(core, prog);
 }
 
 #[test]
 fn ws_persist() {
-    let _ = env_logger::try_init();
-    let mut core = Core::new().unwrap();
-
-    let prog1 = {
-        let s1 = spec("ws-l:127.0.0.1:45914").unwrap();
-        let s2 = spec("literal:qwert4y").unwrap();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        websocat.serve(
-            core.handle(),
-            std::rc::Rc::new(|e| {
-                eprintln!("{}", e);
-                panic!();
-            }),
-        )
-    };
-    let prog2 = {
-        let s1 = spec("literal:invalid_connection_request").unwrap();
-        let s2 = spec("tcp:127.0.0.1:45914").unwrap();
-        let h2 = core.handle();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        let t = tokio_timer::wheel().build();
-        let delay = t.sleep(std::time::Duration::new(0, 200_000_000))
-            .map_err(|_| ());
-
-        delay.and_then(|()| {
-            websocat.serve(
-                h2,
-                std::rc::Rc::new(|e| {
-                    eprintln!("{}", e);
-                    panic!();
-                }),
-            )
-        })
-    };
-    let prog3 = {
-        let s1 = spec("ws://127.0.0.1:45914/ololo").unwrap();
-        let s2 = spec("assert:qwert4y").unwrap();
-        let h2 = core.handle();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        let t = tokio_timer::wheel().build();
-        let delay = t.sleep(std::time::Duration::new(0, 200_000_000))
-            .map_err(|_| ());
-
-        delay.and_then(|()| {
-            websocat.serve(
-                h2,
-                std::rc::Rc::new(|e| {
-                    eprintln!("{}", e);
-                    panic!();
-                }),
-            )
-        })
-    };
+    prepare!(core);
+    let prog1 = wt!(core,
+        "ws-l:127.0.0.1:45914",
+        "literal:qwert4y",
+        noopts,
+        nodelay);
+    let prog2 = wt!(core,
+        "literal:invalid_connection_request",
+        "tcp:127.0.0.1:45914",
+        noopts,
+        delay=200 );
+    let prog3 = wt!(core,
+        "ws://127.0.0.1:45914/ololo",
+        "assert:qwert4y",
+        noopts,
+        delay=400 );
 
     core.handle().spawn(prog1);
     core.handle().spawn(prog2);
     let prog = prog3;
-    core.run(prog).map_err(|()| panic!()).unwrap();
+    run!(core, prog);
 }
+
 
 #[test]
 #[cfg(unix)]
 fn unix() {
-    let _ = env_logger::try_init();
-    let mut core = Core::new().unwrap();
-
-    let prog1 = {
-        let s1 = spec("literal:qwert3y").unwrap();
-        let s2 = spec("unix-l:zxc").unwrap();
-
-        let mut opts: websocat::Options = Default::default();
-        opts.unlink_unix_socket = true;
-        let websocat = WebsocatConfiguration { opts, s1, s2 };
-
-        websocat.serve(
-            core.handle(),
-            std::rc::Rc::new(|e| {
-                eprintln!("{}", e);
-                panic!();
-            }),
-        )
-    };
-    let prog2 = {
-        let s1 = spec("unix-c:zxc").unwrap();
-        let s2 = spec("assert:qwert3y").unwrap();
-        let h2 = core.handle();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        let t = tokio_timer::wheel().build();
-        let delay = t.sleep(std::time::Duration::new(0, 200_000_000))
-            .map_err(|_| ());
-
-        delay.and_then(|()| {
-            websocat.serve(
-                h2,
-                std::rc::Rc::new(|e| {
-                    eprintln!("{}", e);
-                    panic!();
-                }),
-            )
-        })
-    };
+    prepare!(core);
+    let prog1 = wt!(core, 
+        "literal:qwert3y", 
+        "unix-l:zxc",
+        opts=Options { unlink_unix_socket: true, ..dflt() },
+        nodelay);
+    let prog2 = wt!(core, 
+        "unix-c:zxc", 
+        "assert:qwert3y", 
+        noopts, 
+        delay=200);
 
     let prog = prog1.join(prog2);
-    core.run(prog).map_err(|()| panic!()).unwrap();
+    run!(core, prog);
     let _ = ::std::fs::remove_file("zxc");
 }
 
 #[test]
 #[cfg(any(target_os = "linux", target_os = "android"))]
 fn abstract_() {
-    let _ = env_logger::try_init();
-    let mut core = Core::new().unwrap();
-
-    let prog1 = {
-        let s1 = spec("literal:qwert4y").unwrap();
-        let s2 = spec("abstract-l:zxc").unwrap();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        websocat.serve(
-            core.handle(),
-            std::rc::Rc::new(|e| {
-                eprintln!("{}", e);
-                panic!();
-            }),
-        )
-    };
-    let prog2 = {
-        let s1 = spec("abstract-c:zxc").unwrap();
-        let s2 = spec("assert:qwert4y").unwrap();
-        let h2 = core.handle();
-
-        let websocat = WebsocatConfiguration {
-            opts: Default::default(),
-            s1,
-            s2,
-        };
-
-        let t = tokio_timer::wheel().build();
-        let delay = t.sleep(std::time::Duration::new(0, 200_000_000))
-            .map_err(|_| ());
-
-        delay.and_then(|()| {
-            websocat.serve(
-                h2,
-                std::rc::Rc::new(|e| {
-                    eprintln!("{}", e);
-                    panic!();
-                }),
-            )
-        })
-    };
+    prepare!(core);
+    let prog1 = wt!(core,
+        "literal:qwert4y",
+        "abstract-l:zxc",
+        noopts,
+        nodelay);
+    let prog2 = wt!(core,
+        "abstract-c:zxc",
+        "assert:qwert4y",
+        noopts,
+        delay=200);
 
     let prog = prog1.join(prog2);
-    core.run(prog).map_err(|()| panic!()).unwrap();
+    run!(core, prog);
 }
