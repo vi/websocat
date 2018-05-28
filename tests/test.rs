32028c38a01bc5a3c1934ae27e7b5811412ea837
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-05-28 03:33:38 +0300

    Fix the test

--- tests/test.rs
+++ tests/test.rs
@@ -15,51 +15,51 @@ fn dflt() -> Options {
 }
 
 macro_rules! wt {
-    ($core:ident, $s1:expr, $s2:expr,noopts,delay = $ms:expr) => {
-        wt!($core, $s1, $s2, opts = Default::default(), delay = $ms);
-    };
-    ($core:ident, $s1:expr, $s2:expr,noopts,nodelay) => {
-        wt!($core, $s1, $s2, opts = Default::default(), nodelay);
-    };
-    ($core:ident, $s1:expr, $s2:expr,opts = $opts:expr,delay = $ms:expr) => {{
+    ($core:ident, $s1:expr, $s2:expr,delay = $ms:expr, $($rest:tt)*) => {{
         let s1 = spec($s1).unwrap();
         let s2 = spec($s2).unwrap();
         let h2 = $core.handle();
-
-        let websocat = WebsocatConfiguration {
-            opts: $opts,
-            s1,
-            s2,
-        };
-
+        
         let t = tokio_timer::wheel().build();
         let delay = t
             .sleep(std::time::Duration::new(0, $ms * 1_000_000))
             .map_err(|_| ());
 
-        delay.and_then(|()| wt!(wss, websocat, h2))
+        delay.and_then(|()| wt!(stage2, h2, s1, s2, $($rest)*) )
     }};
-    ($core:ident, $s1:expr, $s2:expr,opts = $opts:expr,nodelay) => {{
+    ($core:ident, $s1:expr, $s2:expr,nodelay,$($rest:tt)*) => {{
         let s1 = spec($s1).unwrap();
         let s2 = spec($s2).unwrap();
         let h2 = $core.handle();
 
+        wt!(stage2, h2, s1, s2, $($rest)*)
+    }};
+    (stage2, $h2:ident, $s1:ident, $s2:ident, noopts,$($rest:tt)*) => {
+        wt!(stage2, $h2, $s1, $s2, opts=Default::default(),$($rest)*)
+    };
+    (stage2, $h2:ident, $s1:ident, $s2:ident, opts=$opts:expr,$($rest:tt)*) => {{
+        
         let websocat = WebsocatConfiguration {
             opts: $opts,
-            s1,
-            s2,
+            $s1,
+            $s2,
         };
 
-        wt!(wss, websocat, h2)
-    }};
-    (wss, $websocat:ident, $h2:ident) => {
-        $websocat.serve(
+        websocat.serve(
             $h2,
-            std::rc::Rc::new(|e| {
-                eprintln!("{}", e);
-                panic!();
-            }),
+            wt!(stage3, $($rest)*),
         )
+    }};
+    (stage3, errpanic,) => {
+        std::rc::Rc::new(|e| {
+            eprintln!("{}", e);
+            panic!();
+        })
+    };
+    (stage3, errignore,) => {
+        std::rc::Rc::new(|_| {
+            
+        })
     };
 }
 
@@ -78,9 +78,16 @@ macro_rules! run {
 #[test]
 fn trivial() {
     prepare!(core);
-    let prog = wt!(core, "literal:qwerty", "assert:qwerty", noopts, nodelay);
+    let prog = wt!(core,
+        "literal:qwerty", 
+        "assert:qwerty", 
+        nodelay, 
+        noopts,
+        errpanic,
+    );
     run!(core, prog);
 }
+
 
 #[test]
 fn tcp() {
@@ -89,15 +96,17 @@ fn tcp() {
         core,
         "literal:qwert2y",
         "tcp-l:127.0.0.1:45912",
+        nodelay,
         noopts,
-        nodelay
+        errpanic,
     );
     let prog2 = wt!(
         core,
         "tcp:127.0.0.1:45912",
         "assert:qwert2y",
+        delay = 200,
         noopts,
-        delay = 200
+        errpanic,
     );
 
     let prog = prog1.join(prog2);
@@ -111,15 +120,17 @@ fn ws() {
         core,
         "literal:qwert3y",
         "ws-l:127.0.0.1:45913",
+        nodelay,
         noopts,
-        nodelay
+        errpanic,
     );
     let prog2 = wt!(
         core,
         "ws://127.0.0.1:45913/ololo",
         "assert:qwert3y",
+        delay = 200,
         noopts,
-        delay = 200
+        errpanic,
     );
 
     let prog = prog1.join(prog2);
@@ -133,22 +144,25 @@ fn ws_persist() {
         core,
         "ws-l:127.0.0.1:45914",
         "literal:qwert4y",
+        nodelay,
         noopts,
-        nodelay
+        errignore,
     );
     let prog2 = wt!(
         core,
         "literal:invalid_connection_request",
         "tcp:127.0.0.1:45914",
+        delay = 200,
         noopts,
-        delay = 200
+        errpanic,
     );
     let prog3 = wt!(
         core,
         "ws://127.0.0.1:45914/ololo",
         "assert:qwert4y",
+        delay = 400,
         noopts,
-        delay = 400
+        errpanic,
     );
 
     core.handle().spawn(prog1);
@@ -165,13 +179,20 @@ fn unix() {
         core,
         "literal:qwert3y",
         "unix-l:zxc",
+        nodelay,
         opts = Options {
             unlink_unix_socket: true,
             ..dflt()
         },
-        nodelay
+        errpanic,
     );
-    let prog2 = wt!(core, "unix-c:zxc", "assert:qwert3y", noopts, delay = 200);
+    let prog2 = wt!(core,
+        "unix-c:zxc", 
+        "assert:qwert3y", 
+        delay = 200, 
+        noopts,
+        errpanic,
+    );
 
     let prog = prog1.join(prog2);
     run!(core, prog);
@@ -182,15 +203,23 @@ fn unix() {
 #[cfg(any(target_os = "linux", target_os = "android"))]
 fn abstract_() {
     prepare!(core);
-    let prog1 = wt!(core, "literal:qwert4y", "abstract-l:zxc", noopts, nodelay);
+    let prog1 = wt!(core, 
+        "literal:qwert4y", 
+        "abstract-l:zxc", 
+        nodelay,
+        noopts, 
+        errpanic,
+    );
     let prog2 = wt!(
         core,
         "abstract-c:zxc",
         "assert:qwert4y",
+        delay = 200,
         noopts,
-        delay = 200
+        errpanic,
     );
 
     let prog = prog1.join(prog2);
     run!(core, prog);
 }
+
