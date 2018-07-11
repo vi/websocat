2faa0cbe79f4495e05a2811431587be006a3be95
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-07-11 20:39:43 +0300

    Refactor http_serve

--- src/ws_server_peer.rs
+++ src/ws_server_peer.rs
@@ -2,9 +2,6 @@ extern crate hyper;
 extern crate websocket;
 
 
-use self::hyper::http::h1::Incoming;
-use self::hyper::method::Method;
-use self::hyper::uri::RequestUri;
 use self::hyper::uri::RequestUri::AbsolutePath;
 
 use self::websocket::WebSocketError;
@@ -14,16 +11,14 @@ use futures::stream::Stream;
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use std::fs::File;
+use ::options::StaticFile;
 
 use self::websocket::server::upgrade::async::IntoWs;
-use super::trivial_peer::get_literal_peer_now;
 
 use super::readdebt::{DebtHandling, ReadDebt};
 use super::ws_peer::{Mode1, PeerForWs, WsReadWrapper, WsWriteWrapper};
 use super::{box_up_err, io_other_error, BoxedNewPeerFuture, Peer};
 use super::{ConstructParams, PeerConstructor, Specifier};
-use super::options::StaticFile;
 
 #[derive(Debug)]
 pub struct WsServer<T: Specifier>(pub T);
@@ -115,113 +110,8 @@ WebSocket abstract-namespaced UNIX socket server. [A]
 "#
 );
 
-/* 
-
-     if x == "" {
-                Err("Specify underlying protocol for ws-l:")?;
-            }
-            if let Some(c) = x.chars().next() {
-                if c.is_numeric() || c == '[' {
-                    // Assuming user uses old format like ws-l:127.0.0.1:8080
-                    return spec(&("ws-l:tcp-l:".to_owned() + x));
-                }
-            }
-            boxup(super::ws_server_peer::WsUpgrade(spec(x)?))
-*/
-
-const BAD_REQUEST :&'static [u8] = b"HTTP/1.1 400 Bad Reqeust\r\nServer: websocat\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nOnly WebSocket connections are welcome here\n";
-
-const NOT_FOUND: &'static [u8] = b"HTTP/1.1 404 Not Found\r\nServer: websocat\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nURI does not match any -F option and is not a WebSocket connection.\n";
-
-const NOT_FOUND2: &'static [u8] = b"HTTP/1.1 500 Not Found\r\nServer: websocat\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nFailed to open the file on server side.\n";
-
-const BAD_METHOD :&'static [u8] = b"HTTP/1.1 400 Bad Reqeust\r\nServer: websocat\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nHTTP method should be GET\n";
-
-const BAD_URI_FORMAT :&'static [u8] = b"HTTP/1.1 400 Bad Reqeust\r\nServer: websocat\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nURI should be an absolute path\n";
-
-fn get_static_file_reply(len: Option<u64>, ct: &str) -> Vec<u8> {
-    let mut q = Vec::with_capacity(256);
-    q.extend_from_slice(b"HTTP/1.1 200 OK\r\nServer: websocat\r\nContent-Type: ");
-    q.extend_from_slice(ct.as_bytes());
-    q.extend_from_slice(b"\r\n");
-    if let Some(x) = len {
-        q.extend_from_slice(b"Content-Length: ");
-        q.extend_from_slice(format!("{}",x).as_bytes());
-        q.extend_from_slice(b"\r\n");
-    }
-    q.extend_from_slice(b"\r\n");
-    q
-}
-
-fn http_serve(
-        p:Peer, 
-        incoming:Option<Incoming<(Method, RequestUri)>>,
-        serve_static_files: Rc<Vec<StaticFile>>,
-) -> Box<Future<Item=(), Error=()>> {
-    let mut serve_file = None;
-    let content = if serve_static_files.is_empty() {
-        BAD_REQUEST.to_vec()
-    } else {
-        if let Some(inc) = incoming {
-            info!("HTTP-serving {:?}", inc.subject);
-            if inc.subject.0 == Method::Get {
-                match inc.subject.1 {
-                    AbsolutePath(x) => {
-                        let mut reply = None;
-                        for sf in &*serve_static_files {
-                            if sf.uri == x {
-                                match File::open(&sf.file) {
-                                    Ok(f) => {
-                                        let fs = match f.metadata() {
-                                            Err(_) => None,
-                                            Ok(x) => Some(x.len()),
-                                        };
-                                        reply = Some(get_static_file_reply(
-                                            fs, &sf.content_type));
-                                        serve_file = Some(f);
-                                    },
-                                    Err(_) => {
-                                        reply = Some(NOT_FOUND2.to_vec());
-                                    },
-                                }
-                            }
-                        }
-                        reply.unwrap_or(NOT_FOUND.to_vec())
-                    },
-                    _ => BAD_URI_FORMAT.to_vec(),
-                }
-            } else {
-                BAD_METHOD.to_vec()
-            }
-        } else {
-            BAD_REQUEST.to_vec()
-        }
-    };
-    let reply = get_literal_peer_now(content);
-    
-    let co = super::my_copy::CopyOptions {
-        buffer_size: 1024,
-        once: false,
-        stop_on_reader_zero_read: true,
-    };
-    
-    if let Some(f) = serve_file {
-        Box::new(
-        super::my_copy::copy(reply, p.1, co)
-        .map_err(drop)
-        .and_then(move |(_len,_,conn)| {
-            let co2 = super::my_copy::CopyOptions {
-                buffer_size: 65536,
-                once: false,
-                stop_on_reader_zero_read: true,
-            };
-            let wr = super::file_peer::ReadFileWrapper(f);
-            super::my_copy::copy(wr, conn, co2).map(|_|()).map_err(drop)
-        }))
-    } else {
-        Box::new(super::my_copy::copy(reply, p.1, co).map(|_|()).map_err(drop))
-    }
-}
+#[path = "http_serve.rs"]
+pub mod http_serve;
 
 pub fn ws_upgrade_peer(
     inner_peer: Peer,
@@ -236,7 +126,7 @@ pub fn ws_upgrade_peer(
     > = step1.into_ws();
     let step3 = step2
         .or_else(|(innerpeer, hyper_incoming, _bytesmut, e)| {
-            http_serve(innerpeer.0, hyper_incoming, serve_static_files)
+            http_serve::http_serve(innerpeer.0, hyper_incoming, serve_static_files)
             .then(|_|
                 err(WebSocketError::IoError(io_other_error(e)))
             )
