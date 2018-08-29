c1c9d1ed91e62e256809b9926a409d2d7c6a47a9
Vitaly _Vi Shukela <vi0oss@gmail.com>
2018-08-30 02:24:15 +0300

    Implement tls: overlay

--- src/ssl_peer.rs
+++ src/ssl_peer.rs
@@ -3,13 +3,19 @@ use futures::future::{err, ok, Future};
 
 use std::rc::Rc;
 
-use super::{box_up_err, peer_strerr, BoxedNewPeerFuture, Peer};
+use super::{box_up_err, peer_strerr, BoxedNewPeerFuture, Peer, Result};
 use super::{ConstructParams, L2rUser, PeerConstructor, Specifier};
 use tokio_io::io::{read_exact, write_all};
 
 use std::io::Write;
 use std::net::{IpAddr, Ipv4Addr};
 
+extern crate tokio_tls;
+extern crate native_tls;
+extern crate readwrite;
+
+use self::tokio_tls::TlsConnectorExt;
+use self::native_tls::TlsConnector;
 
 #[derive(Debug)]
 pub struct TlsConnect<T: Specifier>(pub T);
@@ -17,14 +23,14 @@ impl<T: Specifier> Specifier for TlsConnect<T> {
     fn construct(&self, cp: ConstructParams) -> PeerConstructor {
         let inner = self.0.construct(cp.clone());
         inner.map(move |p, l2r| {
-            ssl_connect(p, l2r)
+            ssl_connect(p, l2r, None)
         })
     }
     specifier_boilerplate!(noglobalstate has_subspec);
     self_0_is_subspecifier!(proxy_is_multiconnect);
 }
 specifier_class!(
-    name = TclConnectClass,
+    name = TlsConnectClass,
     target = TlsConnect,
     prefixes = ["ssl-c","ssl:","tls:","ssl-connect:","tls-connect:","c-ssl:","connect-ssl","c-tls:","connect-tls:"],
     arg_handling = subspec,
@@ -42,10 +48,35 @@ For a user-friendly solution, see --socks5 command-line option
 "#
 );
 
+use tokio_io::AsyncRead;
 
 pub fn ssl_connect(
     inner_peer: Peer,
     _l2r: L2rUser,
+    dom: Option<&str>,
 ) -> BoxedNewPeerFuture {
-    Box::new(ok(inner_peer))
+    let squashed_peer = readwrite::ReadWriteAsync::new(inner_peer.0, inner_peer.1);
+    
+    fn gettlsc() -> native_tls::Result<TlsConnector> {
+        Ok(TlsConnector::builder()?.build()?)
+    }
+    let tls = if let Ok(x) = gettlsc() {
+        x
+    } else {
+        return peer_strerr("Failed to initialize TLS");
+    };
+    
+    if let Some(dom) = dom {
+        Box::new(tls.connect_async(dom, squashed_peer).map_err(box_up_err).and_then(move |tls_stream| {
+            info!("Connected to TLS");
+            let (r,w) = tls_stream.split();
+            ok(Peer::new(r,w))
+        }))
+    } else {
+        Box::new(tls.danger_connect_async_without_providing_domain_for_certificate_verification_and_server_name_indication(squashed_peer).map_err(box_up_err).and_then(move |tls_stream| {
+            warn!("Connected to TLS without proper verification of certificate. Use --tls-domain option.");
+            let (r,w) = tls_stream.split();
+            ok(Peer::new(r,w))
+        }))
+    }
 }
