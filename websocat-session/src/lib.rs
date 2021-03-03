02d6669f2bcfce11662b06a192350473a5688724
Vitaly _Vi Shukela <vi0oss@gmail.com>
2021-03-04 01:21:01 +0300

    Ever more accurate waiting

--- websocat-session/src/lib.rs
+++ websocat-session/src/lib.rs
@@ -3,24 +3,36 @@ pub mod copy;
 /// this ball is passed around from session to session
 struct Ball {
     /// session number
-    i : usize,
+    i: usize,
 
     /// Receiver is at session with i=0.
     /// Sender is passed around from one sesion to another.
     /// If it is ever dropped, the first task (i=0) would know
-    /// that is safe to exit
+    /// that is safe to exit, as no multiconn-ignited sessions can be created
     vigilance_tx: Option<tokio::sync::oneshot::Sender<()>>,
+
+    /// AtomicUsize counts currently running parallel sessions (through the read lock)
+    /// Write lock is only used to wait when all other sessions quit, so we are safe
+    /// to exit from the first session
+    ctr: std::sync::Arc<tokio::sync::RwLock<std::sync::atomic::AtomicUsize>>,
 }
 
-
-fn rerun(c: websocat_api::Session, continuation: Option<websocat_api::AnyObject>, ball : Ball) -> tokio::task::JoinHandle<()> {
+fn rerun(
+    c: websocat_api::Session,
+    continuation: Option<websocat_api::AnyObject>,
+    ball: Ball,
+) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
         let _ = run_impl(c, continuation, ball).await;
     })
 }
 
-#[tracing::instrument(name="websocat_session::run", level="debug", skip(c,continuation,ball),  fields(i=tracing::field::display(ball.i)), err)]
-async fn run_impl(c: websocat_api::Session, continuation: Option<websocat_api::AnyObject>, ball:Ball) -> websocat_api::Result<()> {
+#[tracing::instrument(name="session", level="debug", skip(c,continuation,ball),  fields(i=tracing::field::display(ball.i)), err)]
+async fn run_impl(
+    c: websocat_api::Session,
+    continuation: Option<websocat_api::AnyObject>,
+    ball: Ball,
+) -> websocat_api::Result<()> {
     let first_sesion = continuation.is_none();
     if first_sesion {
         tracing::info!("Running a Websocat session");
@@ -37,25 +49,33 @@ async fn run_impl(c: websocat_api::Session, continuation: Option<websocat_api::A
 
     let c2 = c.clone();
     let (vigilance_tx, vigilance_rx) = match ball.vigilance_tx {
-        Some(tx) => {
-            (tx, None)
-        }
+        Some(tx) => (tx, None),
         None => {
             let (tx, rx) = tokio::sync::oneshot::channel::<()>();
-            (tx, Some(rx)) 
+            (tx, Some(rx))
         }
     };
-    
+
     let i = ball.i;
+    let ctr2 = ball.ctr.clone();
     let multiconn = websocat_api::ServerModeContext {
         you_are_called_not_the_first_time: continuation,
         call_me_again_with_this: Box::new(move |cont| {
-            rerun(c2, Some(cont), Ball {
-                i: i+1,
-                vigilance_tx: Some(vigilance_tx),
-            });
+            rerun(
+                c2,
+                Some(cont),
+                Ball {
+                    i: i + 1,
+                    vigilance_tx: Some(vigilance_tx),
+                    ctr: ctr2,
+                },
+            );
         }),
     };
+
+    let readlock = ball.ctr.read().await;
+    let parallel = readlock.fetch_add(1, std::sync::atomic::Ordering::SeqCst)+1;
+    tracing::debug!("Now running {} parallel sessions", parallel);
 
     let p1: websocat_api::Bipipe = c.nodes[c.left].run(rc1, Some(multiconn)).await.unwrap();
 
@@ -66,25 +86,56 @@ async fn run_impl(c: websocat_api::Session, continuation: Option<websocat_api::A
         globals: c.global_things.clone(),
     };
 
-    let p2 : websocat_api::Bipipe = c.nodes[c.right].run(rc2, None).await.unwrap();
+    let p2: websocat_api::Bipipe = c.nodes[c.right].run(rc2, None).await.unwrap();
 
-    let (mut r,mut w) = match (p1.r, p2.w) {
-        (websocat_api::Source::ByteStream(r), websocat_api::Sink::ByteStream(w)) => (r,w),
+    let (mut r, mut w) = match (p1.r, p2.w) {
+        (websocat_api::Source::ByteStream(r), websocat_api::Sink::ByteStream(w)) => (r, w),
         _ => panic!(),
     };
 
     let bytes = copy::copy(&mut r, &mut w).await.unwrap();
     tracing::info!("Finished Websocat session. Processed {} bytes", bytes);
+
+
+    let parallel = readlock.fetch_sub(1, std::sync::atomic::Ordering::SeqCst)-1;
+    tracing::debug!("Now running {} parallel sessions", parallel);
+
+    drop(readlock);
+
     if let Some(vigilance_rx) = vigilance_rx {
-        tracing::debug!("This looks like the first sesion. Waiting for possible ongoing connections to finish.");
+        tracing::debug!(
+            "This looks like the first sesion. Waiting for the listener to finish listening."
+        );
         let _ = vigilance_rx.await;
-        tracing::debug!("No more pending connections to be listened. Assuming it is safe to exit now");
+        tracing::debug!(
+            "No more pending connections to be listened. Now checking for running parallel sessions."
+        );
+        
+        let writelock = ball.ctr.write().await;
+        if writelock.load(std::sync::atomic::Ordering::SeqCst) != 0 {
+            tracing::error!("Somehow obtained write lock while there are also parallel sessions running?");
+            futures::future::pending::<()>().await;
+        }
+        
+        tracing::debug!(
+            "No more parallel sessions. Should be safe to exit now."
+        );
     } else {
         tracing::debug!("Safe to just exit this session, as it is not the first one");
     }
     Ok(())
 }
 
-pub fn run(c: websocat_api::Session) -> impl std::future::Future<Output=websocat_api::Result<()>> {
-  run_impl(c, None, Ball { i: 0, vigilance_tx: None })
+pub fn run(
+    c: websocat_api::Session,
+) -> impl std::future::Future<Output = websocat_api::Result<()>> {
+    run_impl(
+        c,
+        None,
+        Ball {
+            i: 0,
+            vigilance_tx: None,
+            ctr: std::sync::Arc::new(tokio::sync::RwLock::new(std::sync::atomic::AtomicUsize::new(0))),
+        },
+    )
 }
