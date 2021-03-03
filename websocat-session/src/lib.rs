e115b0ec99a0567673a80aec283df9b5cf5557e8
Vitaly _Vi Shukela <vi0oss@gmail.com>
2021-03-04 00:56:32 +0300

    Don't exit when first connection exits

--- websocat-session/src/lib.rs
+++ websocat-session/src/lib.rs
@@ -1,15 +1,28 @@
 pub mod copy;
 
+/// this ball is passed around from session to session
+struct Ball {
+    /// session number
+    i : usize,
 
-fn rerun(c: websocat_api::Session, continuation: Option<websocat_api::AnyObject>) {
-    tokio::spawn(async move {
-        let _ = run_impl(c, continuation).await;
-    });
+    /// Receiver is at session with i=0.
+    /// Sender is passed around from one sesion to another.
+    /// If it is ever dropped, the first task (i=0) would know
+    /// that is safe to exit
+    vigilance_tx: Option<tokio::sync::oneshot::Sender<()>>,
 }
 
-//#[tracing::instrument(name="websocat_session::run", level="debug", skip(c), err)]
-async fn run_impl(c: websocat_api::Session, continuation: Option<websocat_api::AnyObject>) -> websocat_api::Result<()> {
-    if continuation.is_none() {
+
+fn rerun(c: websocat_api::Session, continuation: Option<websocat_api::AnyObject>, ball : Ball) -> tokio::task::JoinHandle<()> {
+    tokio::spawn(async move {
+        let _ = run_impl(c, continuation, ball).await;
+    })
+}
+
+#[tracing::instrument(name="websocat_session::run", level="debug", skip(c,continuation,ball),  fields(i=tracing::field::display(ball.i)), err)]
+async fn run_impl(c: websocat_api::Session, continuation: Option<websocat_api::AnyObject>, ball:Ball) -> websocat_api::Result<()> {
+    let first_sesion = continuation.is_none();
+    if first_sesion {
         tracing::info!("Running a Websocat session");
     } else {
         tracing::info!("Running additional Websocat session");
@@ -23,10 +36,24 @@ async fn run_impl(c: websocat_api::Session, continuation: Option<websocat_api::A
     };
 
     let c2 = c.clone();
+    let (vigilance_tx, vigilance_rx) = match ball.vigilance_tx {
+        Some(tx) => {
+            (tx, None)
+        }
+        None => {
+            let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+            (tx, Some(rx)) 
+        }
+    };
+    
+    let i = ball.i;
     let multiconn = websocat_api::ServerModeContext {
         you_are_called_not_the_first_time: continuation,
         call_me_again_with_this: Box::new(move |cont| {
-            rerun(c2, Some(cont))
+            rerun(c2, Some(cont), Ball {
+                i: i+1,
+                vigilance_tx: Some(vigilance_tx),
+            });
         }),
     };
 
@@ -48,9 +75,16 @@ async fn run_impl(c: websocat_api::Session, continuation: Option<websocat_api::A
 
     let bytes = copy::copy(&mut r, &mut w).await.unwrap();
     tracing::info!("Finished Websocat session. Processed {} bytes", bytes);
+    if let Some(vigilance_rx) = vigilance_rx {
+        tracing::debug!("This looks like the first sesion. Waiting for possible ongoing connections to finish.");
+        let _ = vigilance_rx.await;
+        tracing::debug!("No more pending connections to be listened. Assuming it is safe to exit now");
+    } else {
+        tracing::debug!("Safe to just exit this session, as it is not the first one");
+    }
     Ok(())
 }
 
 pub fn run(c: websocat_api::Session) -> impl std::future::Future<Output=websocat_api::Result<()>> {
-  run_impl(c, None)
+  run_impl(c, None, Ball { i: 0, vigilance_tx: None })
 }
