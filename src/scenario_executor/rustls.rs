e7247386f24c660c3f11472161437396bcf7c2e3
Vitaly _Vi Shukela <vi0oss@gmail.com>
2025-03-14 23:27:05 +0100

    -k option for rustls

--- src/scenario_executor/rustls.rs
+++ src/scenario_executor/rustls.rs
@@ -1,7 +1,7 @@
 use anyhow::bail;
 use bytes::BytesMut;
 use rhai::{Dynamic, Engine, FnPtr, NativeCallContext};
-use rustls::pki_types::ServerName;
+use rustls::{client::danger::ServerCertVerifier, pki_types::ServerName, SignatureScheme};
 use std::sync::Arc;
 use tokio_rustls::TlsConnector;
 
@@ -18,23 +18,86 @@ use super::{
     utils1::RhResult,
 };
 
+#[derive(Debug)]
+struct DummyVerifier;
+
+impl ServerCertVerifier for DummyVerifier {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &rustls::pki_types::CertificateDer<'_>,
+        _intermediates: &[rustls::pki_types::CertificateDer<'_>],
+        _server_name: &ServerName<'_>,
+        _ocsp_response: &[u8],
+        _now: rustls::pki_types::UnixTime,
+    ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
+        Ok(rustls::client::danger::ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        _message: &[u8],
+        _cert: &rustls::pki_types::CertificateDer<'_>,
+        _dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        Ok(rustls::client::danger::HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        _message: &[u8],
+        _cert: &rustls::pki_types::CertificateDer<'_>,
+        _dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        Ok(rustls::client::danger::HandshakeSignatureValid::assertion())
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+        vec![
+            SignatureScheme::RSA_PKCS1_SHA1,
+            SignatureScheme::ECDSA_SHA1_Legacy,
+            SignatureScheme::RSA_PKCS1_SHA256,
+            SignatureScheme::ECDSA_NISTP256_SHA256,
+            SignatureScheme::RSA_PKCS1_SHA384,
+            SignatureScheme::ECDSA_NISTP384_SHA384,
+            SignatureScheme::RSA_PKCS1_SHA512,
+            SignatureScheme::ECDSA_NISTP521_SHA512,
+            SignatureScheme::RSA_PSS_SHA256,
+            SignatureScheme::RSA_PSS_SHA384,
+            SignatureScheme::RSA_PSS_SHA512,
+            SignatureScheme::ED25519,
+            SignatureScheme::ED448,
+        ]
+    }
+}
+
 //@ doc(hidden)
 //@ Create environment for using TLS clients.
 fn tls_client_connector(_ctx: NativeCallContext, opts: Dynamic) -> RhResult<Arc<TlsConnector>> {
     debug!("tls_client_connector");
     #[derive(serde::Deserialize)]
     struct Opts {
-
+        #[serde(default)]
+        danger_accept_invalid_certs: bool,
+        #[serde(default)]
+        danger_accept_invalid_hostnames: bool,
     }
-    let _opts: Opts = rhai::serde::from_dynamic(&opts)?;
+    let opts: Opts = rhai::serde::from_dynamic(&opts)?;
     debug!("options parsed");
 
     let mut root_cert_store = rustls::RootCertStore::empty();
     root_cert_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
 
-    let config = rustls::ClientConfig::builder()
-        .with_root_certificates(root_cert_store)
-        .with_no_client_auth();
+    let config = if opts.danger_accept_invalid_certs && opts.danger_accept_invalid_hostnames {
+        rustls::ClientConfig::builder()
+            .dangerous()
+            .with_custom_certificate_verifier(Arc::new(DummyVerifier))
+            .with_no_client_auth()
+    } else {
+        rustls::ClientConfig::builder()
+            .with_root_certificates(root_cert_store)
+            .with_no_client_auth()
+    };
+
     let connector = TlsConnector::from(Arc::new(config));
 
     Ok(Arc::new(connector))
