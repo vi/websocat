2ef334c40f6a566abd9a7b346b18e33b7c919817
Vitaly _Vi Shukela <vi0oss@gmail.com>
2025-03-21 22:59:38 +0100

    Use strum for lints

--- src/scenario_planner/linter.rs
+++ src/scenario_planner/linter.rs
@@ -1,9 +1,22 @@
 use super::types::{Endpoint, SocketType, WebsocatInvocation};
 
+#[derive(strum_macros::Display)]
 pub enum Lint {
+    #[strum(
+        to_string = "--oneshot may fail to properly exit process when stdin is used (https://github.com/tokio-rs/tokio/issues/2466). If only stdout is needed, add -u; or add --exit-after-one-session to force exit after serving the connection."
+    )]
     StdoutOneshotWithoutExit,
+    #[strum(
+        to_string = "--global-timeout-ms may fail to properly exit process when stdin is used (https://github.com/tokio-rs/tokio/issues/2466). You may want to also add the --global-timeout-force-exit option."
+    )]
     StdoutGlobalTimeoutWithoutExit,
+    #[strum(
+        to_string = "Listening specifier should be the first specifier (at the left side) in command line. It would server only one connection if found at the right side. Use --oneshot if this is intended"
+    )]
     ListenerAtTheWrongSide,
+    #[strum(
+        to_string = "You have specified separator-related option (--separator or --separator-n or --null-terminated), but Websocat is operating in bytestream-oriented mode where separators are not used. Consider `-t` option or `lines:` overlay."
+    )]
     UnusedSeparatorOption,
 }
 
@@ -44,24 +57,5 @@ impl WebsocatInvocation {
         }
 
         ret
-    }
-}
-
-impl std::fmt::Display for Lint {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Lint::StdoutOneshotWithoutExit => {
-                "--oneshot may fail to properly exit process when stdin is used (https://github.com/tokio-rs/tokio/issues/2466). If only stdout is needed, add -u; or add --exit-after-one-session to force exit after serving the connection.".fmt(f)
-            }
-            Lint::StdoutGlobalTimeoutWithoutExit => {
-                "--global-timeout-ms may fail to properly exit process when stdin is used (https://github.com/tokio-rs/tokio/issues/2466). You may want to also add the --global-timeout-force-exit option.".fmt(f)
-            }
-            Lint::ListenerAtTheWrongSide => {
-                "Listening specifier should be the first specifier (at the left side) in command line. It would server only one connection if found at the right side. Use --oneshot if this is intended".fmt(f)
-            }
-            Lint::UnusedSeparatorOption => {
-                "You have specified separator-related option (--separator or --separator-n or --null-terminated), but Websocat is operating in bytestream-oriented mode where separators are not used. Consider `-t` option or `lines:` overlay.".fmt(f)
-            },
-        }
     }
 }
