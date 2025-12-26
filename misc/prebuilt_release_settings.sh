370210ad4fa3c1684773694492e234e5b61efd3d
Vitaly _Vi Shukela <vi0oss@gmail.com>
2025-12-26 21:48:43 +0100

    Adjust the release artifacts script

--- misc/prebuilt_release_settings.sh
+++ misc/prebuilt_release_settings.sh
@@ -2,7 +2,16 @@ targetsettings() {
   EXTRA_CARGO_FLAGS="--features=vendored_openssl"
   case "$1" in
      i686-unknown-linux-musl)
-          EXTRA_CARGO_FLAGS="--no-default-features"
+          EXTRA_CARGO_FLAGS="--no-default-features --features=socketoptions"
+     ;;
+     arm-unknown-linux-musleabi)
+          EXTRA_CARGO_FLAGS="--no-default-features --features=rustls,socketoptions"
+     ;;
+     loongarch64-unknown-linux-musl)
+          #EXTRA_CARGO_FLAGS="--no-default-features --features=rustls,socketoptions"
+     ;;
+     wasm32-wasip1)
+          SKIP=1
      ;;
   esac
 }
